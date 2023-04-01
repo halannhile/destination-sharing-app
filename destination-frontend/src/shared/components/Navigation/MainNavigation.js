7320353fd58b556b6655a20d0de11a24a36f8a2a
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-04-01 17:25:11 +1100

    animation for opening and closing side drawer

--- destination-frontend/src/shared/components/Navigation/MainNavigation.js
+++ destination-frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop'
 
 // to make things clickable
 import { Link } from 'react-router-dom';
@@ -10,32 +12,45 @@ import { Link } from 'react-router-dom';
 import './MainNavigation.css';
 
 function MainNavigation(props) {
-  return (
-    <React.Fragment>
-        
-        <SideDrawer>
-            <nav className="main-navigation__drawer-nav">
-                <NavLinks/>
-            </nav>
-        </SideDrawer>
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-        <MainHeader>
-            <button className="main-navigation__menu-btn">
-                <span/>
-                <span/>
-                <span/>
-            </button>
-            <h1 className="main-navigation__title">
-                {/* always navigate back to home page when clicking on name of the app YourDestinations */}
-                <Link to="/">YourDestinations</Link>
-            </h1>
-            <nav className="main-navigation__header-nav">
-                <NavLinks/>
-            </nav>
-        </MainHeader>
+    const openDrawer = () => {
+        setDrawerIsOpen(true)
+    }
+    
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    }
 
-    </React.Fragment>
-  )
+    return (
+        <React.Fragment>
+            {drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
+            {drawerIsOpen && (
+            <SideDrawer>
+                <nav className="main-navigation__drawer-nav">
+                    <NavLinks/>
+                </nav>
+            </SideDrawer>) 
+            }
+
+            <MainHeader>
+                {/* burger button to open side drawer */}
+                <button className="main-navigation__menu-btn" onClick={openDrawer}>
+                    <span/>
+                    <span/>
+                    <span/>
+                </button>
+                <h1 className="main-navigation__title">
+                    {/* always navigate back to home page when clicking on name of the app YourDestinations */}
+                    <Link to="/">YourDestinations</Link>
+                </h1>
+                <nav className="main-navigation__header-nav">
+                    <NavLinks/>
+                </nav>
+            </MainHeader>
+
+        </React.Fragment>
+    )
 }
 
 export default MainNavigation
--- destination-frontend/src/shared/components/Navigation/SideDrawer.js
+++ destination-frontend/src/shared/components/Navigation/SideDrawer.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
 
 import './SideDrawer.css'
 
 function SideDrawer(props) {
-  return (
+  
+  const content = 
     <aside className="side-drawer">
        {props.children} 
     </aside>
+
+  return (
+    // tell React which content to render where
+    ReactDOM.createPortal(
+      content, 
+      document.getElementById('drawer-hook'))
   )
 }
 
