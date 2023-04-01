a69cd32e4772f4da5acc11bd61d550ad2c5f5cf9
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-04-01 16:56:08 +1100

    sideDrawer

--- destination-frontend/src/shared/components/Navigation/MainNavigation.js
+++ destination-frontend/src/shared/components/Navigation/MainNavigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
 
 // to make things clickable
 import { Link } from 'react-router-dom';
@@ -10,20 +11,30 @@ import './MainNavigation.css';
 
 function MainNavigation(props) {
   return (
-    <MainHeader>
-        <button className="main-navigation__menu-btn">
-            <span/>
-            <span/>
-            <span/>
-        </button>
-        <h1 className="main-navigation__title">
-            {/* always navigate back to home page when clicking on name of the app YourDestinations */}
-            <Link to="/">YourDestinations</Link>
-        </h1>
-        <nav>
-            <NavLinks/>
-        </nav>
-    </MainHeader>
+    <React.Fragment>
+        
+        <SideDrawer>
+            <nav className="main-navigation__drawer-nav">
+                <NavLinks/>
+            </nav>
+        </SideDrawer>
+
+        <MainHeader>
+            <button className="main-navigation__menu-btn">
+                <span/>
+                <span/>
+                <span/>
+            </button>
+            <h1 className="main-navigation__title">
+                {/* always navigate back to home page when clicking on name of the app YourDestinations */}
+                <Link to="/">YourDestinations</Link>
+            </h1>
+            <nav className="main-navigation__header-nav">
+                <NavLinks/>
+            </nav>
+        </MainHeader>
+
+    </React.Fragment>
   )
 }
 
