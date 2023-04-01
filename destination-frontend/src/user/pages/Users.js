bfab0c8154851b055d91b6ed9ec8a1b400bb66e5
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-04-01 15:00:16 +1100

    styling for useritem and userslist

--- destination-frontend/src/user/pages/Users.js
+++ destination-frontend/src/user/pages/Users.js
@@ -4,7 +4,13 @@ import UsersList from '../components/UsersList'
 
 function Users() {
 
-  const USERS = [{id: 'u1', name: 'Nhi Le', image: 'http://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?d=identicon', places: 3 }];
+  const USERS = [
+    {
+      id: 'u1', 
+      name: 'Nhi Le', 
+      image: 'http://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?d=identicon', 
+      places: 3 
+    }];
 
   return (
     <UsersList items={USERS}/>
