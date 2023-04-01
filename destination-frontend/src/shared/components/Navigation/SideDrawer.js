2c3f8c0178dc5a2c7acb9824a62c053f28706c99
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-04-01 17:41:45 +1100

    finished animation for side drawer

--- destination-frontend/src/shared/components/Navigation/SideDrawer.js
+++ destination-frontend/src/shared/components/Navigation/SideDrawer.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group'
 
 import './SideDrawer.css'
 
 function SideDrawer(props) {
   
   const content = 
-    <aside className="side-drawer">
+    // note: 200 milliseconds animation, classNames with s
+    <CSSTransition 
+      in={props.show} 
+      timeout={200} 
+      classNames="slide-in-left"
+      mountOnEnter
+      mountOnExit
+      >
+    <aside className="side-drawer" onClick={props.onClick}>
        {props.children} 
     </aside>
+    </CSSTransition>
 
   return (
     // tell React which content to render where
