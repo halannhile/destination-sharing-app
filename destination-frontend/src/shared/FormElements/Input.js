e0e5acfbca3f19ea1814e0aee3b4e9666290e35a
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-04-04 21:30:10 +1000

    input/form control set up via Input.js

--- destination-frontend/src/shared/FormElements/Input.js
+++ destination-frontend/src/shared/FormElements/Input.js
@@ -2,10 +2,21 @@ import React from 'react'
 
 import './Input.css'
 
-function Input() {
-  return (
-    <div>Input</div>
-  )
+function Input(props) {
+    // if element is input, return <input/>, else return <textarea/>
+    const element = props.element === 'input' ? (
+            <input id={props.id} type={props.type} placeholder={props.placeholder}/> 
+        ) : (
+            // if props.rows is not available, default value is 3
+            <textarea id={props.id} rows={props.rows || 3} />
+        );
+        
+    return (
+        <div className={`form-control`}>
+            <label htmlFor={props.id}>{props.label}</label>
+            {element}
+        </div>
+    )
 }
 
 export default Input
