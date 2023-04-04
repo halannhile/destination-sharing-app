88c994ed190382570ec38768bd5958fbfdff06d0
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-04-04 21:36:30 +1000

    form input field

--- destination-frontend/src/places/components/PlaceItem.js
+++ destination-frontend/src/places/components/PlaceItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 
 import Card from '../../shared/components/UIElements/Card'
-import Button from '../../shared/FormElements/Button'
+import Button from '../../shared/components/FormElements/Button'
 import Modal from '../../shared/components/UIElements/Modal'
 import Map from '../../shared/components/UIElements/Map'
 import './PlaceItem.css'
--- destination-frontend/src/places/pages/NewPlace.js
+++ destination-frontend/src/places/pages/NewPlace.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import Input from '../../shared/components/FormElements/Input'
+import './NewPlace.css'
+
 function NewPlace() {
   return (
     <form className="place-form">
 
+      {/* if don't specify element="input", Input.js will render <textarea/> instead of <input/> */}
+      <Input element="input" type="text" label="Title" />
     </form>
   )
 }
