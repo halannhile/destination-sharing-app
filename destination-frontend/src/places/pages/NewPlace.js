a14a05eb11196b93a414dae576a1baee2d18993e
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-04-07 22:00:20 +1000

    added Description field in NewPlace form

--- destination-frontend/src/places/pages/NewPlace.js
+++ destination-frontend/src/places/pages/NewPlace.js
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react'
 
 import Input from '../../shared/components/FormElements/Input'
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators'
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators'
 import './NewPlace.css'
 
 function NewPlace() {
@@ -9,18 +9,32 @@ function NewPlace() {
   const titleInputHandler = useCallback((id, value, isValid) => {
     }, []);
 
+  const descriptionInputHandler = useCallback((id, value, isValid) => {
+    }, []);
+
   return (
     <form className="place-form">
 
       {/* if don't specify element="input", Input.js will render <textarea/> instead of <input/> */}
       <Input 
+        id="title"
         element="input" 
         type="text" 
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid input" 
         onInput={titleInputHandler} />
+
+      <Input 
+        id="description"
+        element="textarea" 
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)." 
+        onInput={descriptionInputHandler} />
+
     </form>
+    
   )
 }
 
