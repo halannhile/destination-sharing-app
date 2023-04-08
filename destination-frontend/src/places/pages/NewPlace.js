eb93ee6653d972c8eb1768614d6a46e21e217a0b
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-04-08 13:53:01 +1000

    added formReducer to NewPlace

--- destination-frontend/src/places/pages/NewPlace.js
+++ destination-frontend/src/places/pages/NewPlace.js
@@ -1,17 +1,79 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useReducer } from 'react'
 
 import Input from '../../shared/components/FormElements/Input'
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators'
 import './NewPlace.css'
 
+const formReducer = (state, action) => {
+  switch(action.type) {
+    case 'INPUT_CHANGE':
+      
+      // helper variable: 
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+
+        // this logic makes sure we always take the previous value of formIsValid
+        // and combine it with the new validity value
+        // false+false=false, false+true=false, true+true=true
+        // so, if either Title or Description field is invalid, form is invalid
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+
+      return {
+        ...state,
+        
+        inputs: {
+          ...state.inputs,
+          // dynamic assignment: if we dispatch an action with inputId="Title"
+          // then we'll only update Title with new value and validity
+          // while leaving description untouched
+          [action.inputId]: { value: action.value, isValid: action.isValid }
+        },
+
+        // overall form validity 
+        isValid: formIsValid
+      };
+
+    default: 
+      return state;
+  }
+};
+
+
 function NewPlace() {
+
+  useReducer(formReducer, {
+
+    // initial state of inputs, which contain title and description
+    inputs: {
+
+      // initial state of title
+      title: {
+        value: '',
+        isValid: false
+      },
+
+      // initial state of description
+      description: {
+        value: '',
+        isValid: false
+      }
+    },
+
+    // initial overall state of form
+    isValid: false
+  });
 
   const titleInputHandler = useCallback((id, value, isValid) => {
     }, []);
 
   const descriptionInputHandler = useCallback((id, value, isValid) => {
     }, []);
-
+ 
   return (
     <form className="place-form">
 
