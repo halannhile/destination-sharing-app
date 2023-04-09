09662b154cc084a943afc353f78a11de3e19b090
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-04-09 14:04:48 +1000

    styling for user log in form

--- destination-frontend/src/user/pages/Auth.js
+++ destination-frontend/src/user/pages/Auth.js
@@ -4,7 +4,6 @@ import Input from '../../shared/components/FormElements/Input';
 import { VALIDATOR_MINLENGTH, VALIDATOR_EMAIL } from '../../shared/util/validators';
 import Button from '../../shared/components/FormElements/Button';
 import { useForm } from '../../shared/hooks/form-hook';
-import './Auth.css'
 import './UserAuthForm.css';
 
 function Auth() {
@@ -27,37 +26,39 @@ function Auth() {
         false
     );
 
-    const userSubmitHandler = event => {
+    const authSubmitHandler = event => {
         event.preventDefault();
         console.log(formState.inputs); // will later send this to the backend
     }
 
   return (
-    <form className="user-auth-form" onSubmit={userSubmitHandler}>
+    <div>
+      <h2 className="user-auth-form__header center">LOG IN TO YOUR ACCOUNT</h2>
+      <form className="user-auth-form" onSubmit={authSubmitHandler}>
 
-      {/* if don't specify element="input", Input.js will render <textarea/> instead of <input/> */}
-      <Input 
-        id="email"
-        element="input" 
-        type="text" 
-        label="Email"
-        validators={[VALIDATOR_EMAIL()]}
-        errorText="Please enter a valid email address" 
-        onInput={inputHandler} />
+        {/* if don't specify element="input", Input.js will render <textarea/> instead of <input/> */}
+        <Input 
+          id="email"
+          element="input" 
+          type="email" 
+          label="Email"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email address" 
+          onInput={inputHandler} />
 
-      <Input 
-        id="password"
-        element="input" 
-        type="text"  
-        label="Password"
-        validators={[VALIDATOR_MINLENGTH(8)]}
-        errorText="Please enter a valid password (at least 8 characters)" 
-        onInput={inputHandler} />
+        <Input 
+          id="password"
+          element="input" 
+          type="text"  
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(8)]}
+          errorText="Please enter a valid password (at least 8 characters)" 
+          onInput={inputHandler} />
 
-      {/* button is disabled when form is invalid */}
-      <Button type="submit" disabled={!formState.isValid}>LOG IN</Button>
-
-    </form>
+        {/* button is disabled when form is invalid */}
+        <Button type="submit" disabled={!formState.isValid}>LOG IN</Button>
+      </form>
+    </div>
   )
 }
 
