b59230f798e1c7be0fefd23299b240108f2a3db5
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-04-15 19:14:52 +1000

    changed NavLinks to include auth context

--- destination-frontend/src/shared/components/Navigation/NavLinks.js
+++ destination-frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,30 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 
+import { AuthContext } from '../../context/auth-context'
 import './NavLinks.css'
 
+
 function NavLinks(props) {
-  return (
-    <ul className="nav-links">
+    // auth holds isLoggedIn property, login method and logout method
+    const auth = useContext(AuthContext)
+    return (
+        <ul className="nav-links">
 
-        <li>
-            <NavLink to="/" exact>ALL USERS</NavLink>
-        </li>
+            <li>
+                <NavLink to="/" exact>ALL USERS</NavLink>
+            </li>
 
-        <li>
-            <NavLink to="/u1/places">MY PLACES</NavLink>
-        </li>
+            {/* only show MY PLACES when user is logged in */}
+            {auth.isLoggedIn && 
+                <li>
+                    <NavLink to="/u1/places">MY PLACES</NavLink>
+                </li>
+            }        
 
-        <li>
-            <NavLink to="/places/new">ADD PLACE</NavLink>
-        </li>
+            {/* only show ADD PLACE when user is logged in */}
+            {auth.isLoggedIn &&             
+                <li>
+                    <NavLink to="/places/new">ADD PLACE</NavLink>
+                </li>
+            }
 
-        <li>
-            <NavLink to="/auth">AUTHENTICATE</NavLink>
-        </li>
+            {/* only show AUTHENTICATE when user is NOT logged in */}
+            {!auth.isLoggedIn &&
+                <li>
+                    <NavLink to="/auth">AUTHENTICATE</NavLink>
+                </li>
+            }
 
-    </ul>
-  )
+        </ul>
+    )
 }
 
 export default NavLinks
