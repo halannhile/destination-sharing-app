2cd5c8ee612a9e5c6d48997bef074b969de3433e
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-04-22 15:37:13 +1000

    express practice

--- node-express-practice/app.js
+++ node-express-practice/app.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use((req, res, next) => {
     let body = '';
+    
     // end listener: execute once we're done parsing the req's body
     req.on('end', () => {
         const userName = body.split('=')[1]
@@ -13,18 +14,20 @@ app.use((req, res, next) => {
         // adding next() inside 'end' listener so we'll only move on to the next one once finish parsing the body
         next();
     });
+    
     req.on('data', chunk => {
         body += chunk;
     });
 });
 
 app.use((req, res, next) => {
+
     if (req.body) {
         // return here so we won't render the form if req.body is available
         return res.send('<h1>' + req.body.name + '</h1>')
     }
+
     res.send('<form method="POST"><input type="text" name="username"><button>CREATE USER</button></form>')
 });
-
 // with Express, no need to use createServer
 app.listen(5000);
