03176455e28959fcfa105e1a897715d8efaa19c1
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-05-11 20:02:15 +1000

    added post and get to app.js

--- node-express-practice/app.js
+++ node-express-practice/app.js
@@ -1,33 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((req, res, next) => {
-    let body = '';
-    
-    // end listener: execute once we're done parsing the req's body
-    req.on('end', () => {
-        const userName = body.split('=')[1]
-        if (userName) {
-        // add a body property to the documentation of the req object 
-        req.body = {name: userName};}
-        // adding next() inside 'end' listener so we'll only move on to the next one once finish parsing the body
-        next();
-    });
-    
-    req.on('data', chunk => {
-        body += chunk;
-    });
+// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded( {extended: false} ))
+
+app.post('/user', (req, res, next) => {
+    res.send('<h1>User: ' + req.body.username + '</h1>')
+})
+
+// app.use('/users')
+
+app.get('/', (req, res, next) => {
+    res.send(
+        '<form action="/user" method="POST"><input type="text" name="username"><button>CREATE USER</button></form>'
+        )
 });
 
-app.use((req, res, next) => {
-
-    if (req.body) {
-        // return here so we won't render the form if req.body is available
-        return res.send('<h1>User: ' + req.body.name + '</h1>')
-    }
-
-    res.send('<form method="POST"><input type="text" name="username"><button>CREATE USER</button></form>')
-});
 // with Express, no need to use createServer
 app.listen(5000);
