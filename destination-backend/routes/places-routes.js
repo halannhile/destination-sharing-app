c282cd705d8e171719c02380ca721df0f38f8f2c
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-05-17 22:01:35 +1000

    user specific route

--- destination-backend/routes/places-routes.js
+++ destination-backend/routes/places-routes.js
@@ -16,15 +16,26 @@ const DUMMY_PLACES = [
     }
 ];
 
-// register a route: 
+// register a route for api/places/placeId: 
 router.get('/:pid', (req, res, next) => {
     const placeId = req.params.pid; // { pid: 'p1' }
     const place = DUMMY_PLACES.find(p => {
-        return p.id == placeId;
+        return p.id === placeId;
     });
     console.log('GET request in Places');
     // send back a response with some json
     res.json({place: place}); // or can also shortern it to {place}
+});
+
+// register a route for /users/userId:
+router.get('/users/:uid', (req, res, next) => {
+    const userId = req.params.uid;
+
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId;
+    });
+
+    res.json({ place });
 });
 
 // export the router here to ue in app.js
