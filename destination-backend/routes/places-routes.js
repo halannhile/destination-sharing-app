23a97124a8d4b3217de8d6a666fcf29e373a0816
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-05-19 20:46:23 +1000

    using HttpError in places-routes

--- destination-backend/routes/places-routes.js
+++ destination-backend/routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -25,9 +27,7 @@ router.get('/:pid', (req, res, next) => {
 
     // return 404 error if place not found
     if (!place) {
-        const error = new Error('Could not find a place for the provided id.')
-        error.code = 404;
-        throw error
+        throw new HttpError('Could not find a place for the provided id.', 404);
     }
 
     // send back a response with some json
@@ -43,12 +43,8 @@ router.get('/user/:uid', (req, res, next) => {
     });
 
     if (!place) {
-        if (!place) {
-            const error = new Error('Could not find a place for the provided user id.')
-            error.code = 404;
-            return next(error)
+            return next(new Error('Could not find a place for the provided user id.', 404));
         }
-    }
 
     res.json({ place });
 });
