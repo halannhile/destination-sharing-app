e585a9cdfe4ed86411aacf205900077b838a20bc
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-05-19 21:13:02 +1000

    places-controller

---
+++ destination-backend/controllers/places-controller.js
@@ -0,0 +1,46 @@
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Sydney Opera House',
+        description: 'An architectural icon of Sydney',
+        location: {
+            lat: -33.85662900846622, 
+            lng: 151.21531387971248
+        },
+        address: 'Bennelong Point, Sydney NSW 2000, Australia',
+        creator: 'u1'
+    }
+];
+
+// methods to create this function: 
+// 1. function getPlaceById() { ... }
+// 2. const getPlaceById = function() { ... }
+
+const getPlaceById = (req, res, next) => {
+    const placeId = req.params.pid; // { pid: 'p1' }
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId;
+    });
+
+    // return 404 error if place not found
+    if (!place) {
+        throw new HttpError('Could not find a place for the provided id.', 404);
+    }
+
+    // send back a response with some json
+    res.json({place: place}); // or can also shortern it to {place}
+}
+
+const getPlaceByUserId = (req, res, next) => {
+    const userId = req.params.uid;
+
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId;
+    });
+
+    if (!place) {
+            return next(new Error('Could not find a place for the provided user id.', 404));
+        }
+
+    res.json({ place });
+}
