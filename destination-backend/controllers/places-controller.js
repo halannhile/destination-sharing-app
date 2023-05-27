e173b07daf29d58a3fb05e4c57cf65f30a75c254
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-05-27 21:50:43 +1000

    updatePlace

--- destination-backend/controllers/places-controller.js
+++ destination-backend/controllers/places-controller.js
@@ -68,7 +68,27 @@ const createPlace = (req, res, next) => {
     res.status(201).json({place: createPlace});
 };
 
+const updatePlace = (req, res, next) => {
+    const { title, description } = req.body;
+    const placeId = req.params.pid;
+
+    // identify the place to be updated
+    const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+    const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+    updatedPlace.title = title;
+    updatedPlace.description = description;
+
+    DUMMY_PLACES[placeIndex] = updatedPlace;
+
+    res.status(200).json({place: updatedPlace});
+};
+
+const deletePlace = (req, res, next) => {};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
+
 
