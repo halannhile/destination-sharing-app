0fc9dfd33e43fa36e50e4eb9e9eb6ba5a3bb9e93
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-05-28 18:03:41 +1000

    usersControllers

--- destination-backend/routes/users-routes.js
+++ destination-backend/routes/users-routes.js
@@ -8,10 +8,10 @@ const router = express.Router();
 router.get('/', usersControllers.getUsers);
 
 // POST route for sign up: 
-// router.post('/signup', );
+router.post('/signup', usersControllers.signup);
 
 // POST route for log in: 
-// router.post('/login', );
+router.post('/login', usersControllers.login);
 
 // export the router here to ue in app.js
 module.exports = router;
