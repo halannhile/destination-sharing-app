cafecb8026af094db9ebdd0cf86db94030051e8d
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-05-28 13:01:03 +1000

    deletePlace in places-controller

--- destination-backend/controllers/places-controller.js
+++ destination-backend/controllers/places-controller.js
@@ -2,7 +2,7 @@ const uuid = require('uuid');
 
 const HttpError = require('../models/http-error');
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
     {
         id: 'p1',
         title: 'Sydney Opera House',
@@ -83,7 +83,16 @@ const updatePlace = (req, res, next) => {
     res.status(200).json({place: updatedPlace});
 };
 
-const deletePlace = (req, res, next) => {};
+const deletePlace = (req, res, next) => {
+    // getting id of place we want to delete
+    const placeId = req.params.pid;
+
+    // replace DUMMY_PLACES array with only places that are not to be deleted
+    DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+    
+    res.status(200).json({message: 'Deleted place.'})
+}
+
 
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
