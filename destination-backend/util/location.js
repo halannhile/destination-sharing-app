dc1e712445dd7c319f14bd4922564397908e7560
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-06-01 22:19:13 +1000

    location.js

---
+++ destination-backend/util/location.js
@@ -0,0 +1,10 @@
+const API_KEY = 'AIzaSyB1E4SW3K-FrCG7rMIXtjOcw_GDuZk4YPQ';
+
+// more on async: https://academind.com/tutorials/callbacks-vs-promises-vs-rxjs-vs-async-awaits
+async function getCoordsForAddress(address) {
+    return { 
+        lat: 40.7484474, 
+        lng: -73.9871516
+    };
+}
+
