646b536e7083afa8a5c3dec13e6276214b4be1d2
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-06-01 23:22:45 +1000

    use getCoordsForPlace in places-controllers

--- destination-backend/controllers/places-controllers.js
+++ destination-backend/controllers/places-controllers.js
@@ -2,6 +2,7 @@ const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const getCoordsForAddress = require('../util/location');
 
 let DUMMY_PLACES = [
     {
@@ -12,7 +13,7 @@ let DUMMY_PLACES = [
             lat: -33.85662900846622, 
             lng: 151.21531387971248
         },
-        address: 'Bennelong Point, Sydney NSW 2000, Australia',
+        address: 'Bennelon4g Point, Sydney NSW 2000, Australia',
         creator: 'u1'
     },
     {
@@ -62,17 +63,24 @@ const getPlacesByUserId = (req, res, next) => {
     res.json({ places });
 }
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
     // checks on req's body: 
     const errors = validationResult(req);
     console.log(errors);
     if (!errors.isEmpty()) {
-        throw new HttpError('Invalid inputs, please check your data.', 422);
+        next(new HttpError('Invalid inputs, please check your data.', 422));
     };
     
     // object destructuring: 
-    const { title, description, coordinates, address, creator } = req.body;
+    const { title, description, address, creator } = req.body;
     
+    let coordinates;
+    try {
+        coordinates = await getCoordsForAddress(address);
+    } catch (error) {
+        return next(error);
+    }
+
     const createdPlace = {
         id: uuidv4(),
         title, // shortcut for title: title
--- destination-backend/util/location.js
+++ destination-backend/util/location.js
@@ -1,13 +1,36 @@
+const axios = require('axios');
+
+const HttpError = require('../models/http-error');
+
 require('dotenv').config();
 const apiKey = process.env.API_KEY;
 
 // more on async: https://academind.com/tutorials/callbacks-vs-promises-vs-rxjs-vs-async-awaits
 async function getCoordsForAddress(address) {
+
     // solution when we have no api key: 
     // return { 
     //     lat: 40.7484474, 
     //     lng: -73.9871516
     // };
 
+    const respons = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}
+        &key=${apiKey}`
+        );
 
-}
+    const data = response.data;
+    
+    // when address can't be found: 
+    if (!data || data.status === 'ZERO_RESULTS') {
+        const error = new HttpError('Could not find coordinates for the specified address.', 422);
+
+        throw error;
+    }
+
+    const coordinates = data.results[0].geometry.location;
+
+    return coordinates;
+};
+
+module.exports = getCoordsForAddress;
