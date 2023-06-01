d32d18a38b453bffc964c2cc1f88c0f0ab8e095b
Nhi Le <55796146+halannhile@users.noreply.github.com>
2023-06-01 22:40:05 +1000

    Update location.js

--- destination-backend/util/location.js
+++ destination-backend/util/location.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
+
 // more on async: https://academind.com/tutorials/callbacks-vs-promises-vs-rxjs-vs-async-awaits
 async function getCoordsForAddress(address) {
     // solution when we have no api key: 
