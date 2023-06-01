265f3ce6a1dc0d4bfa6737f97f04ef43b551bf0f
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-06-01 22:22:13 +1000

    gitignore api_key

--- destination-backend/util/location.js
+++ destination-backend/util/location.js
@@ -1,3 +1,4 @@
+const API_KEY = 'AIzaSyB1E4SW3K-FrCG7rMIXtjOcw_GDuZk4YPQ';
 
 // more on async: https://academind.com/tutorials/callbacks-vs-promises-vs-rxjs-vs-async-awaits
 async function getCoordsForAddress(address) {
