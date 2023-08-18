c67fc49507a806b5acf016e361e03b963c03d9b4
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-08-18 15:41:18 -0700

    fixed geocoding

--- destination-backend/util/location.js
+++ destination-backend/util/location.js
@@ -2,35 +2,32 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-require('dotenv').config();
-const apiKey = process.env.API_KEY;
+const API_KEY = 'AIzaSyDwCwZycUUZkvhstBcXZi6rIQNVwbxYhIg';
 
-// more on async: https://academind.com/tutorials/callbacks-vs-promises-vs-rxjs-vs-async-awaits
 async function getCoordsForAddress(address) {
+  // return {
+  //   lat: 40.7484474,
+  //   lng: -73.9871516
+  // };
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${API_KEY}`
+  );
 
-    // solution when we have no api key: 
-    // return { 
-    //     lat: 40.7484474, 
-    //     lng: -73.9871516
-    // };
+  const data = response.data;
 
-    const respons = await axios.get(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}
-        &key=${apiKey}`
-        );
+  if (!data || data.status === 'ZERO_RESULTS') {
+    const error = new HttpError(
+      'Could not find location for the specified address.',
+      422
+    );
+    throw error;
+  }
 
-    const data = response.data;
-    
-    // when address can't be found: 
-    if (!data || data.status === 'ZERO_RESULTS') {
-        const error = new HttpError('Could not find coordinates for the specified address.', 422);
+  const coordinates = data.results[0].geometry.location;
 
-        throw error;
-    }
-
-    const coordinates = data.results[0].geometry.location;
-
-    return coordinates;
-};
+  return coordinates;
+}
 
 module.exports = getCoordsForAddress;
