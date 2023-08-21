15b34033708abe8a26d1290878c750491db99232
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-08-21 13:58:42 -0700

    linking app to backend

--- mongodb-refresher/app.js
+++ mongodb-refresher/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mongoPractice = require('./mongo');
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/products');
+app.post('/products', mongoPractice.createProduct);
 
-app.get('/products');
+// Add a handler function for the GET /products endpoint
+app.get('/products', (req, res) => {
+  // Handle the logic for fetching and sending products here
+  // For example:
+  res.json({ message: 'GET /products endpoint works' });
+});
 
-app.listen(3000);
-
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
