2b7b94173a1accc492baada4845b36292ebdb928
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-08-21 15:02:19 -0700

    mongoose

--- mongodb-refresher/app.js
+++ mongodb-refresher/app.js
@@ -9,12 +9,7 @@ app.use(bodyParser.json());
 
 app.post('/products', mongoPractice.createProduct);
 
-// Add a handler function for the GET /products endpoint
-app.get('/products', (req, res) => {
-  // Handle the logic for fetching and sending products here
-  // For example:
-  res.json({ message: 'GET /products endpoint works' });
-});
+app.get('/products', mongoPractice.getProducts);
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
