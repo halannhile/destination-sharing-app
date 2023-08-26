ba4e36f53fa5aedc1710b2dffd5a3a6a8c6d506c
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-08-26 19:03:49 -0400

    mongoose for updatePlace

--- destination-backend/controllers/places-controllers.js
+++ destination-backend/controllers/places-controllers.js
@@ -145,25 +145,46 @@ const createPlace = async (req, res, next) => {
     res.status(201).json({place: createdPlace});
 };
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
     // checks on req's body: 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         throw new HttpError('Invalid inputs, please check your data.', 422);
     };
 
+    // get info from req's body
     const { title, description } = req.body;
     const placeId = req.params.pid;
 
     // identify the place to be updated
-    const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
-    const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
-    updatedPlace.title = title;
-    updatedPlace.description = description;
+    let place;
+    try{
+        place = await Place.findById(placeId);
+    } catch (err) {
+        const error = newHttpError(
+            'Something went wrong, could not update place.',
+            500
+        );
+        return next(error);
+    };
 
-    DUMMY_PLACES[placeIndex] = updatedPlace;
+    // updating the place
+    place.title = title;
+    place.description = description;
 
-    res.status(200).json({place: updatedPlace});
+    // storing the updated place
+    try {
+        await place.save()
+    } catch (err) {
+        const error = newHttpError(
+            'Something went wrong, could not update place',
+            500
+        );
+        return next(error);
+    };
+
+
+    res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
 const deletePlace = (req, res, next) => {
