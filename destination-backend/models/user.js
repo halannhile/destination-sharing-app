ee67010dd47222939509402d13f48627f8136818
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-08-26 19:46:44 -0400

    userSchema

---
+++ destination-backend/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator')
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+
+    // using unique here will create an index for each email
+    // speeding up email search when user logs in 
+    email: { type: String, required: true, unique: true },
+    
+    password: { type: String, required: true, minlength: 6 },
+    image: { type: String, required: true },
+    
+    // each user can have multiple places, which will be dynamic
+    // we'll store the IDs allocated to a user here
+    places: { type: String, required: true }
+});
+
+// internal validation to make sure emails are unique (not done by mongoose)
+// condition: only create a new user if the email doesn't exist already
+userSchema.plugin(uniqueValidator);
+
+// naming convention: model 'Place', collection 'places'
+// require inputs: model name 'Place', schema name 'placeSchema' 
+module.exports = mongoose.model('User', userSchema);
